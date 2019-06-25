9229fe4509a4924939fbc40689cab3bd1eca78ba
Hajime Yamasaki Vukelic <hayavuk@gmail.com>
2019-06-25 19:07:20 +0200

    Do not provide the UMD build
    Because the query-string dependency does not provide the UMD version, it
    would be hard to predict what the global would be called. We don't want
    to create problems that we'll need to solve later.

--- rollup.config.js
+++ rollup.config.js
@@ -1,22 +1,9 @@
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
 import pkg from './package.json';
 
 export default [
 	{
 		input: 'src/index.js',
-		output: {
-			name: 'transplexer',
-			file: pkg.browser,
-			format: 'umd',
-		},
-		plugins: [
-			resolve(),
-			commonjs(),
-		],
-	},
-	{
-		input: 'src/index.js',
+    external: ['query-string'],
 		output: [
 			{ file: pkg.main, format: 'cjs' },
 			{ file: pkg.module, format: 'es' }
